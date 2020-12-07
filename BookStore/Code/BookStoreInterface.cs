a59a65a36d4ba38a1e415fa173a6a7a34dbe35d4
Alex Vasil <alexandervasil840@gmail.com>
2020-12-07 13:29:54 -0500

    Added a new class which will store a instance of each object or list of objects to be serialized, don't know if this will work but heres hoping it will

---
+++ BookStore/Code/BookStoreInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Code {
+    [Serializable]
+    class BookStoreInterface {
+        public List<Book> Book { get; set; }
+        public BookStore BookStore { get; set; }
+        public List<Customer> Customer {  get; set; }
+
+        public BookStoreInterface() { }
+
+        public BookStoreInterface(List<Book> book, BookStore bookStore, List<Customer> customer) { }
+    }
+}
